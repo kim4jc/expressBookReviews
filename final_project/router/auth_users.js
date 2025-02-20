e893efe1f01cc05611dbd9cddc005c2c9593bf21
Jacob <kim4jc@mail.uc.edu>
2025-02-20 16:10:46 -0500

    implemented code for authenticated user routes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+const e = require('express');
 const regd_users = express.Router();
 
 let users = [];
@@ -20,20 +21,78 @@ const isValid = (username)=>{
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    // Filter the users array for any user with the same username and password
+    let validUsers = users.filter(user=>{
+        return(user.username === username && user.password === password);
+    });
+    if(validUsers.length > 0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let username = req.body.username;
+    let password = req.body.password;
+
+    //return error if username or password is missing
+    if(!username || !password){
+        return res.status(400).json({message: "Error logging in"});
+    }
+    
+    //authenticate user
+    if(authenticatedUser(username, password)){
+        // Generate JWT access token
+        let accessToken = jwt.sign({data: username}, 'access', {expiresIn: 60 * 60});
+        
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).json({message: "User successfully logged in"});
+    }
+    else{
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn; 
+    let review = req.body.review; 
+    let username = req.user.data; //get username from jwt payload
+
+    if(books[isbn]){
+        books[isbn].reviews[username] = review;
+        return res.status(200).json({message: `${username} has added the review: "${review}" to the book ${books[isbn].title}`})
+    }
+    else{
+        return res.status(404).json({message: `Book with isbn: ${isbn} not found.`})
+    }
 });
+
+//delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let isbn = req.params.isbn;
+    let username = req.user.data;
+    
+    if(books[isbn]){
+        if(books[isbn].reviews[username]){
+            delete books[isbn].reviews[username];
+            return res.status(200).json({message: `Successfully deleted your review on ${books[isbn].title}.`})
+        }
+        else{
+            return res.status(400).json({message: `Review for ${books[isbn].title} not found.`})
+        }
+    }
+    else{
+        return res.status(400).json({message: `Book with isbn: ${isbn} not found.`})
+    }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
